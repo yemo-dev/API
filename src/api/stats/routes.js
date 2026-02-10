c020128d594dfa4335cf800e95107a2fc7796072
yemobyte <yemobyte@gmail.com>
2026-02-09 22:35:02 -0800

    feat: move status config to route definition

--- src/api/stats/routes.js
+++ src/api/stats/routes.js
@@ -5,6 +5,7 @@ export const statsRoute = createRoute({
     method: 'get',
     path: '/api/stats',
     description: 'Get detailed server system information (CPU, RAM, OS, etc)',
+    'x-status': 'ONLINE',
     responses: {
         200: {
             content: {
