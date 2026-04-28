da94a291a78c97addefef39005d7d8760c4d5f6c
miuubyte <miuudev@gmail.com>
2026-04-28 05:05:12 -0700

    refactor: simplify to purely file-based config and set default to modern

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,7 @@
 import { statsRoute, statsHandler } from './stats/index.js'
 import { keyStatusRoute, keyStatusHandler } from './auth/index.js'
-import { adminRouter } from './admin.js'
 
 export const setupRoutes = (app) => {
     app.openapi(statsRoute, statsHandler)
     app.openapi(keyStatusRoute, keyStatusHandler)
-    app.route('/api/admin', adminRouter)
 }
