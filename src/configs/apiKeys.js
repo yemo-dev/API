75682dc825caaa27bedd0aed8f52efc804645e20
miuubyte <miuudev@gmail.com>
2026-04-27 19:07:46 -0700

    feat: add multiple API key tiers for testing

--- src/configs/apiKeys.js
+++ src/configs/apiKeys.js
@@ -4,6 +4,18 @@ export const apiKeys = [
     key: 'a8d9f1c2b3e4a5c6',
     limit: 0,
     windowMs: 10 * 60 * 1000
+  },
+  {
+    name: 'Premium User',
+    key: 'p1r2e3m4i5u6m7k8',
+    limit: 5000,
+    windowMs: 10 * 60 * 1000
+  },
+  {
+    name: 'Basic User',
+    key: 'b1a2s3i4c5k6e7y8',
+    limit: 50,
+    windowMs: 10 * 60 * 1000
   }
 ]
 
